e62c5fcd764ac60f913c2135ca79061b4365affb
María José Fonseca Castro <mfonsecac052@gmail.com>
2024-11-12 22:41:33 +0000

    archivos creados de componentes

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,10 +1,9 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
-import { People } from "../component/cardsComponents.jsx"
+// import { People } from "../component/cardsComponents.jsx"; //CARD DE PEOPLE
 
 export const Home = () => (
 	<div className="text-center mt-5">
-		<People />
+		{/* <People /> //CARD DE PEOPLE */}
 	</div>
 );

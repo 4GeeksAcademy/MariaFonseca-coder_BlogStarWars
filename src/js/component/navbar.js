2b9dc9e4663d6e92859849d2c5f25b5ce361da16
María José Fonseca Castro <mfonsecac052@gmail.com>
2024-11-12 21:33:10 +0000

    Logo en navbar, botón listo en diseño de navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,16 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from '../../img/logo.jpg';
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar mb-3" style={{ backgroundColor: '#000000' }}>
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				<img></img>
+				<img
+					src={logo}
+					alt="Logo"
+					style={{ width: '130px', height: '70px' }}
+					className="navbar-brand mb-0 h1"
+				/>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Favorities</button>
+					<div class="btn-group" role="group">
+						<button id="btnGroupDrop1" type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+							Favorites
+						</button>
+						<ul class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+							{/* <li><a class="dropdown-item" href="#">Dropdown link</a></li> //AQUÍ VA LOS PERSONAJES O ASI QUE SE INTEGRAN A FAVORITOS
+							<li><a class="dropdown-item" href="#">Dropdown link</a></li> */}
+						</ul>
+					</div>
 				</Link>
 			</div>
 		</nav>
